30a2c2e57c5ea0342fcd25a50531f65eded4fa40
Markus Gumbel <m.gumbel@hs-mannheim.de>
2018-05-18 16:40:01 +0200

    Introduced Apaches CLI for parsing arguments.Introduced Apaches CLI for parsing arguments.

--- src/main/java/bio/gcat/abcdtool/main/Analyze.java
+++ src/main/java/bio/gcat/abcdtool/main/Analyze.java
@@ -4,6 +4,7 @@ import bio.gcat.abcdtool.analysis.NPletsAnalysis;
 import bio.gcat.abcdtool.sequences.generator.ConditionalProbabilities;
 import bio.gcat.abcdtool.sequences.generator.RandomSeqStringGenerator;
 import bio.gcat.abcdtool.sequences.reader.FastFastaLoader;
+import org.apache.commons.cli.*;
 import org.biojava.nbio.core.sequence.compound.NucleotideCompound;
 import org.biojava.nbio.core.sequence.template.Sequence;
 
@@ -22,85 +23,141 @@ import java.util.Set;
  */
 public class Analyze {
   public static void main(String[] args) throws IOException {
-    if (args.length < 1) {
-      System.out.println("usage: args");
-      System.out.println("1.: <file> (valid path)");
-      System.out.println("2.: random|randomconditional|normal|N (normal is default)");
-      System.out.println("Info:");
-      System.out.println("<file>: a dna sequence in FastA format");
-      System.out.println("random: complete random sequence according to " +
-              "base distribution in homo sapiens, chr. 1");
-      System.out.println("randomconditional: random sequence with conditional " +
-              "probabilities as calculated in the sequence (param 1)");
-      System.out.println("normal: regular analysis (default)");
-      System.out.println("N: Split analysis where sequence is " +
-              "split into N parts.");
-      System.exit(0);
-    }
-    String filePath = args[0];
-    System.out.println("Using Fasta file " + args[0]);
-    long sTime = System.currentTimeMillis();
-    FastFastaLoader fastaFile = new FastFastaLoader(new File(filePath));
-    Map<String, Sequence<NucleotideCompound>> entries =
-            fastaFile.fastaEntries();
-    double msec = (System.currentTimeMillis() - sTime) / (1.0 * entries.size());
-    System.out.println("|entries| = " + entries.size() + " (" + msec + " ms/read)");
 
-    /*
+    long sTime = System.currentTimeMillis();
+    Options options = new Options(); // Command line options.
+
+    OptionGroup sourceOptions = new OptionGroup();
+    sourceOptions.addOption(Option.builder("f")
+            .longOpt("fasta")
+            .required()
+            .hasArg()
+            .desc("sequence in fasta format")
+            .argName("fasta filename")
+            .build());
+    sourceOptions.addOption(Option.builder("i")
+            .longOpt("rndInit")
+            .desc("random sequence with probabilities like in human chr. 1.")
+            .build());
+    sourceOptions.addOption(Option.builder("c")
+            .longOpt("rndCond")
+            .desc("random sequence with conditional probabilities like in human chr. 1.")
+            .build());
+    sourceOptions.addOption(Option.builder("C")
+            .longOpt("rndCondFile")
+            .desc("random sequence with conditional probabilities like in fasta file")
+            .hasArg()
+            .argName("fasta filename")
+            .build());
+    options.addOptionGroup(sourceOptions);
+
+    options.addOption(Option.builder("s")
+            .longOpt("size")
+            .required(false)
+            .hasArg()
+            .desc("length of the random sequence; default is 100,000")
+            .argName("integer")
+            .type(Long.class)
+            .build());
+
+    OptionGroup methodOptions = new OptionGroup();
+    methodOptions.addOption(Option.builder("n")
+            .longOpt("normal")
+            .desc("regular n-plets analysis (default method)")
+            .build());
+    methodOptions.addOption(Option.builder("p")
+            .longOpt("part")
+            .desc("partition based n-plets analysis")
+            .build());
+    options.addOptionGroup(methodOptions);
+
+        /*
     List<Integer> tupleSizes = Arrays.asList(1, 2, 3, 4, 5,
             6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 50);
     */
     List<Integer> tupleSizes = Arrays.asList(1, 3, 25);
-    long size = 100000; // 250000000;
 
-    Set<Map.Entry<String, Sequence<NucleotideCompound>>> g = entries.entrySet();
-    for (Map.Entry<String, Sequence<NucleotideCompound>> map : g) {
-      String sequence = map.getValue().getSequenceAsString();
-      String seqId = map.getKey();
-      System.out.println("Id: " + seqId + ", size: " + sequence.length());
+    // Create the parser:
+    CommandLineParser parser = new DefaultParser();
+    try {
+      // parse the command line arguments
+      CommandLine line = parser.parse(options, args);
 
-      if (args.length >= 2) {
-        if (args[1].equalsIgnoreCase("rndInit")) {
-          System.out.println("Method: random initial; Human Chr. 1 ");
-          seqId = "RndInit-" + sequence;
-          sequence = new RandomSeqStringGenerator().randomSeqString(size);
-          NPletsAnalysis npa = new NPletsAnalysis(seqId, sequence, tupleSizes);
-          npa.analyze();
+      String sizeAsString = line.hasOption("size") ?
+              line.getOptionValue("size") : "100000";
+      long size = Long.valueOf(sizeAsString);
+      String method = "normal";
+      if (line.hasOption("normal")) {
+        method = "method";
+      }
+      if (line.hasOption("part")) {
+        method = "part";
+      }
+
+      if (line.hasOption("fasta")) {
+        System.out.println("Method: sequence like in Fasta file");
+        String filePath = line.getOptionValue("fasta");
+        Map<String, Sequence<NucleotideCompound>> entries =
+                loadFastaFile(filePath);
+        Set<Map.Entry<String, Sequence<NucleotideCompound>>> g = entries.entrySet();
+        for (Map.Entry<String, Sequence<NucleotideCompound>> map : g) {
+          String sequence = map.getValue().getSequenceAsString();
+          String seqId = map.getKey();
+          analyze(seqId, sequence, tupleSizes, method);
         }
-        if (args[1].equalsIgnoreCase("rndCond")) {
-          System.out.println("Method: random conditional; Human Chr. 1 ");
-          seqId = "RndCond-" + seqId;
-          sequence = new RandomSeqStringGenerator().rndCondSeqString(size, true);
-          NPletsAnalysis npa = new NPletsAnalysis(seqId, sequence, tupleSizes);
-          npa.analyze();
-        }
-        if (args[1].equalsIgnoreCase("rndCondFile")) {
-          System.out.println("Method: random conditional like in Fasta file");
+        // ----------------------------------------
+      } else if (line.hasOption("randomInitial")) {
+        System.out.println("Method: random initial; Human Chr. 1 ");
+        String seqId = "RndInit";
+        String sequence = new RandomSeqStringGenerator().randomSeqString(size);
+        analyze(seqId, sequence, tupleSizes, method);
+        // ----------------------------------------
+      } else if (line.hasOption("randomConditionalFromFile")) {
+        String filePath = line.getOptionValue("randomConditionalFromFile");
+        System.out.println("Method: random conditional from file " +
+                filePath);
+        Map<String, Sequence<NucleotideCompound>> entries =
+                loadFastaFile(filePath);
+        Set<Map.Entry<String, Sequence<NucleotideCompound>>> g =
+                entries.entrySet();
+        for (Map.Entry<String, Sequence<NucleotideCompound>> map : g) {
+          String sequence = map.getValue().getSequenceAsString();
           double[][] condProbabilities =
                   new ConditionalProbabilities().
                           createConditionalProbabilityMatrix(sequence);
           sequence = new RandomSeqStringGenerator().
-                  rndCondSeqString(sequence.length(), condProbabilities);
-          NPletsAnalysis npa = new NPletsAnalysis(seqId + "random", sequence, tupleSizes);
-          npa.analyze();
+                  rndCondSeqString(size, condProbabilities);
+          String seqId = map.getKey() + "-RandomCond";
+          analyze(seqId, sequence, tupleSizes, method);
         }
-        if (args[1].equalsIgnoreCase("normal")) {
-          System.out.println("Method: sequence like in Fasta file");
-          NPletsAnalysis npa = new NPletsAnalysis(seqId, sequence, tupleSizes);
-          npa.analyze();
-        }
-        if (args[1].equalsIgnoreCase("N")) {
-          System.out.println("Method: N with sequence like in Fasta file");
-          analyzeN(seqId, sequence, tupleSizes);
-        }
+        // ----------------------------------------
       } else {
-        // TODO redundant
-        System.out.println("Method: sequence like in Fasta file");
-        NPletsAnalysis npa = new NPletsAnalysis(seqId, sequence, tupleSizes);
-        npa.analyze();
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("Analyzer",
+                "Analyzing n-plets in a DNA sequence.",
+                options, "More infos at www.gcat.bio.");
+        return;
       }
+    } catch (ParseException exp) {
+      // Oops, something went wrong:
+      System.err.println("Parsing failed.  Reason: " + exp.getMessage());
     }
     System.out.println("Done in " + ((System.currentTimeMillis() - sTime) / 1000) + " seconds.");
+  }
+
+  private static void analyze(String seqId, String sequence,
+                              List<Integer> tupleSizes,
+                              String method) {
+    switch (method) {
+      case "normal":
+        System.out.println("Id: " + seqId + ", size: " + sequence.length());
+        NPletsAnalysis npa = new NPletsAnalysis(seqId, sequence, tupleSizes);
+        npa.analyze();
+        break;
+      case "part":
+        analyzeN(seqId, sequence, tupleSizes);
+        break;
+    }
   }
 
   /**
@@ -126,9 +183,15 @@ public class Analyze {
       }
     }
   }
+
+  private static Map<String, Sequence<NucleotideCompound>>
+  loadFastaFile(String filePath) {
+    long sTime = System.currentTimeMillis();
+    FastFastaLoader fastaFile = new FastFastaLoader(new File(filePath));
+    Map<String, Sequence<NucleotideCompound>> entries =
+            fastaFile.fastaEntries();
+    double msec = (System.currentTimeMillis() - sTime) / (1.0 * entries.size());
+    System.out.println("|entries| = " + entries.size() + " (" + msec + " ms/read)");
+    return entries;
+  }
 }
-
-
-
-
-
